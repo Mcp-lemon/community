609aa95f32d0e8817a4731b1118a029a35b217b0
996456776@qq.com <mcp996996>
2019-07-23 09:47:30 +0800

    add index and bootstarp

--- src/main/java/com/mcp/study/community/controller/HelloController.java
+++ src/main/java/com/mcp/study/community/controller/HelloController.java
@@ -1,9 +1,7 @@
 package com.mcp.study.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author mcp
@@ -13,9 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
 
     @GetMapping("/hello")
-        public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+        public String hello(){
+        return "index";
     }
 
 
