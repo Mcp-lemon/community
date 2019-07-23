2f0ee174901d89572b1b5ff4e5c84e639d66853b
996456776@qq.com <mcp996996>
2019-07-23 17:23:52 +0800

    add user login cookie

--- src/main/java/com/mcp/study/community/mapper/UserMapper.java
+++ src/main/java/com/mcp/study/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.mcp.study.community.mapper;
 import com.mcp.study.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author mcp
@@ -14,4 +16,6 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("select * from User where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
